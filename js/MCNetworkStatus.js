03a4155c300f77603a963e648e1f08fe99adc804
Zake Aljaberi <42307974+Zake2002V2@users.noreply.github.com>
2025-10-10 08:42:49 -0400

    MC network overhaul

--- js/MCNetworkStatus.js
+++ js/MCNetworkStatus.js
@@ -1,11 +1,17 @@
 $(document).ready(() => {
     const mcnetworkurl = "mcnetwork.zake2002.xyz:19132";
-    const mcmainserver = "mcnetwork.zake2002.xyz:29653";
+    const mcmainserver = "mcnetwork.zake2002.xyz:22002";
+    const mccreativeserver = "mcnetwork.zake2002.xyz:29656";
+    const mclegacyserver = "mcnetwork.zake2002.xyz:29653";
+    const mcoriginalserver = "zxcorporiginalserver.falixsrv.me:34678";
     const mcservers = [
         mcmainserver,
+        mccreativeserver,
+        mclegacyserver,
+        mcoriginalserver,
         "pc.zake2002.xyz:54248",
         "mcnetwork.zake2002.xyz:32568",
-        "mcnetwork.zake2002.xyz:29656",
+        "504768.blockworlds.io:25566",
         "node1.hg-gaming.eu:25565"
     ];
 
@@ -47,7 +53,6 @@ $(document).ready(() => {
             } else {
                 $("#players_num").text(`${mainserver.players?.online} connected`);
             };
-            $("#mcmap").addClass("map");
         } else {
             $("#status").text("Offline").addClass("offline");
 
@@ -70,4 +75,59 @@ $(document).ready(() => {
         $("#status").text("Error").addClass("offline");
         $("#players_num").hide();
     });
+
+    // Fetch creative server status
+    $.getJSON(`https://api.mcsrvstat.us/1/${mccreativeserver}`, (creativeserver) => {
+        if (creativeserver.debug?.ping) {
+            $("#creative_status").text("Online!").addClass("online");
+            if (creativeserver.players?.max != 0) {
+                $("#creative_players_num").text(`${creativeserver.players?.online}/${creativeserver.players?.max} connected`);
+            } else {
+                $("#creative_players_num").text(`${creativeserver.players?.online} connected`);
+            };
+        } else {
+            $("#creative_status").text("Offline").addClass("offline");
+            $("#creative_players_num").hide();
+        }
+    }).fail(() => {
+        $("#creative_status").text("Error").addClass("offline");
+        $("#creative_players_num").hide();
+    });
+
+    // Fetch legacy server status
+    $.getJSON(`https://api.mcsrvstat.us/1/${mclegacyserver}`, (legacyserver) => {
+        if (legacyserver.debug?.ping) {
+            $("#legacy_status").text("Online!").addClass("online");
+            if (legacyserver.players?.max != 0) {
+                $("#legacy_players_num").text(`${legacyserver.players?.online}/${legacyserver.players?.max} connected`);
+            } else {
+                $("#legacy_players_num").text(`${legacyserver.players?.online} connected`);
+            };
+        } else {
+            $("#legacy_status").text("Offline").addClass("offline");
+            $("#legacy_players_num").hide();
+        }
+    }).fail(() => {
+        $("#legacy_status").text("Error").addClass("offline");
+        $("#legacy_players_num").hide();
+    });
+
+        // Fetch original server status
+    $.getJSON(`https://api.mcsrvstat.us/1/${mcoriginalserver}`, (originalserver) => {
+        if (originalserver.debug?.ping) {
+            $("#original_status").text("Online!").addClass("online");
+            if (originalserver.players?.max != 0) {
+                $("#original_players_num").text(`${originalserver.players?.online}/${originalserver.players?.max} connected`);
+            } else {
+                $("#original_players_num").text(`${originalserver.players?.online} connected`);
+            };
+        } else {
+            $("#original_status").text("Offline").addClass("offline");
+            $("#original_players_num").hide();
+        }
+    }).fail(() => {
+        $("#original_status").text("Error").addClass("offline");
+        $("#original_players_num").hide();
+    });
+
 });
