4bf2615b5cb3a619033fac361d46b880076f89ff
Zake Aljaberi <42307974+Zake2002V2@users.noreply.github.com>
2025-11-06 04:02:15 -0500

    Fixed original server ping

--- js/MCNetworkStatus.js
+++ js/MCNetworkStatus.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     const mcmainserver = "mcnetwork.zake2002.xyz:22002";
     const mccreativeserver = "mcnetwork.zake2002.xyz:29656";
     const mclegacyserver = "mcnetwork.zake2002.xyz:29653";
-    const mcoriginalserver = "zxcorporiginalserver.falixsrv.me:34678";
+    const mcoriginalserver = "zxcorporiginalserver.falixsrv.me:65478";
     const mcservers = [
         mcmainserver,
         mccreativeserver,
